cc3df19472cec306e67c1b17358b33f2b24f6927
vipinneemamnit <vipinneemamnnit@gmail.com>
2024-11-16 18:31:33 +0530

    Create 167.TwoSumII-InputArrayIsSorted.cpp

---
+++ 167.TwoSumII-InputArrayIsSorted.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0, r = numbers.size()-1;
+        while(l < r){
+            int sum = numbers[l]+ numbers[r];
+            if(sum == target) return {l+1, r+1};
+            else if(sum < target) l++;
+            else r--;
+        }
+        return {-1,-1};
+    }
+};
