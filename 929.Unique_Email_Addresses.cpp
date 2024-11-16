06b82b8d5406ae99fa5f7d74229cbf98d939026c
vipinneemamnit <vipinneemamnnit@gmail.com>
2024-11-16 18:30:04 +0530

    Create 929.Unique_Email_Addresses.cpp

---
+++ 929.Unique_Email_Addresses.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/unique-email-addresses/description/
+
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+        set<pair<string, string>> uniqueEmails;
+
+        for(const string& email : emails){
+            size_t atPos = email.find('@');
+            string local = email.substr(0, atPos);
+            string domain = email.substr(atPos+1);
+
+            size_t plusPos = local.find('+');
+            if(plusPos != string::npos){
+                local = local.substr(0, plusPos);
+            }
+
+            //local.erase(remove(local.begin(), local.end(), '.'), local.end());
+            auto it = remove(local.begin(), local.end(), '.'); // Move '.' characters to the end
+            local.erase(it, local.end()); // Erase from the new end to the actual end
+            
+            uniqueEmails.insert({local, domain});
+            
+        }
+    return uniqueEmails.size();
+    }
+};
