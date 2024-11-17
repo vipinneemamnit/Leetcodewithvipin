03d3f2934457b63ec762a4ecf7bcf6275501d0e5
vipinneemamnit <vipinneemamnnit@gmail.com>
2024-11-17 09:31:29 +0530

    Create 42.TrappingRainWater.cpp

---
+++ 42.TrappingRainWater.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l = 0, r = height.size()-1;
+        int leftMax = height[0], rightMax = height[r];
+        int res = 0;
+
+        while(l < r){
+            if(leftMax < rightMax){
+                l++;
+                leftMax = max(leftMax, height[l]);
+                res+= leftMax-height[l];    
+            } else {
+                r--;
+                rightMax = max(rightMax, height[r]);
+                res+=rightMax-height[r]; 
+            }
+        }
+        return res;
+    }
+};
